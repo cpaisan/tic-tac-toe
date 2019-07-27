b7a948481168b280e45b5422200318854c13a644
Charlie Paisan <cppaisan@gmail.com>
2019-07-27 13:14:52 -0700

    [chore] Dynamic board size tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { configure, shallow, mount } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+import App from "./App";
+import Board from "components/Board";
+
+configure({ adapter: new Adapter() });
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+it("should update the board size", () => {
+  const wrapper = mount(<App />);
+  expect(wrapper.find(Board).props().boardSize).toEqual(3);
+  expect(wrapper.find(Board).find("button").length).toEqual(9);
+  wrapper.find("input").simulate("change", { target: { value: 8 } });
+  expect(wrapper.find(Board).props().boardSize).toEqual(8);
+  expect(wrapper.find(Board).find("button").length).toEqual(64);
 });
