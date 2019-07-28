68a80f692831945d454467a4fe794304cea8e6e5
Charlie Paisan <cppaisan@gmail.com>
2019-07-27 22:33:32 -0700

    [chore] Add tests for initial game state and updated game state

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,22 +3,75 @@ import ReactDOM from "react-dom";
 import { configure, shallow, mount } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
-import App from "./App";
+import App, { constructGameState, updateGameState } from "./App";
 import Board from "components/Board";
+import BoardTile from "components/BoardTile";
 
 configure({ adapter: new Adapter() });
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe("App", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
 
-it("should update the board size", () => {
-  const wrapper = mount(<App />);
-  expect(wrapper.find(Board).props().boardSize).toEqual(3);
-  expect(wrapper.find(Board).find("button").length).toEqual(9);
-  wrapper.find("input").simulate("change", { target: { value: 8 } });
-  expect(wrapper.find(Board).props().boardSize).toEqual(8);
-  expect(wrapper.find(Board).find("button").length).toEqual(64);
+  it("should update the board size", () => {
+    const wrapper = mount(<App />);
+    expect(wrapper.find(Board).props().boardSize).toEqual(3);
+    expect(wrapper.find(Board).find("button").length).toEqual(9);
+    wrapper.find("input").simulate("change", { target: { value: 8 } });
+    expect(wrapper.find(Board).props().boardSize).toEqual(8);
+    expect(wrapper.find(Board).find("button").length).toEqual(64);
+  });
+
+  it("should switch players' turns after making a move", () => {
+    const wrapper = mount(<App />);
+    // First player should be "X"
+    wrapper
+      .find(BoardTile)
+      .at(0)
+      .simulate("click");
+    expect(
+      wrapper
+        .find(BoardTile)
+        .at(0)
+        .props().value
+    ).toEqual("X");
+    //Second player should be "O"
+    wrapper
+      .find(BoardTile)
+      .at(1)
+      .simulate("click");
+    expect(
+      wrapper
+        .find(BoardTile)
+        .at(1)
+        .props().value
+    ).toEqual("O");
+  });
+
+  it("should construct the initial board state", () => {
+    const boardState = constructGameState(2);
+    const expectedBoardState = [["", ""], ["", ""]];
+    expect(boardState).toEqual(expectedBoardState);
+  });
+
+  it("should update the game state", () => {
+    const initialGameState = constructGameState(2);
+    const setGameState = jest.fn();
+    const setPlayerTurn = jest.fn();
+    const playerTurn = "X";
+    const expectedGameState = [["X", ""], ["", ""]];
+    updateGameState(initialGameState, setGameState, playerTurn, setPlayerTurn)(
+      0,
+      0
+    );
+    // Game state should be updated
+    expect(setGameState.mock.calls.length).toEqual(1);
+    expect(setGameState).toHaveBeenCalledWith(expectedGameState);
+    // Player turn should be updated
+    expect(setPlayerTurn.mock.calls.length).toEqual(1);
+    expect(setPlayerTurn).toHaveBeenCalledWith("O");
+  });
 });
--- src/components/Board/__tests__/Board.test.js
+++ src/components/Board/__tests__/Board.test.js
@@ -3,24 +3,57 @@ import { configure, shallow, mount } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 
 import Board, { constructBoard } from "../Board";
+import BoardTile from "components/BoardTile";
+import { constructGameState, updateGameState } from "App";
 
 configure({ adapter: new Adapter() });
 
 describe("Board", () => {
   it("should render the game board", () => {
-    const wrapper = mount(<Board boardSize={5} />);
+    const gameState = constructGameState(5);
+    const setGameState = jest.fn();
+    const setPlayerTurn = jest.fn();
+    const wrapper = mount(
+      <Board
+        boardSize={5}
+        gameState={gameState}
+        playerTurn="X"
+        updateGameState={updateGameState(
+          gameState,
+          setGameState,
+          "X",
+          setPlayerTurn
+        )}
+      />
+    );
     expect(wrapper.props().boardSize).toEqual(5);
     expect(wrapper.find("button").length).toBe(25);
   });
 
   it("should render the game board without a specified boardSize", () => {
-    const wrapper = mount(<Board />);
+    const gameState = constructGameState();
+    const setGameState = jest.fn();
+    const setPlayerTurn = jest.fn();
+    const wrapper = mount(
+      <Board
+        gameState={gameState}
+        playerTurn="X"
+        updateGameState={updateGameState(
+          gameState,
+          setGameState,
+          "X",
+          setPlayerTurn
+        )}
+      />
+    );
     expect(wrapper.props().boardSize).toEqual(3);
-    expect(wrapper.find("button").length).toEqual(9);
+    expect(wrapper.find(BoardTile).length).toEqual(9);
   });
 
   it("should construct a multidimensional array", () => {
-    const board = constructBoard(10);
+    const gameState = constructGameState(10);
+    const updateGameState = jest.fn();
+    const board = constructBoard(gameState, updateGameState, "X");
     expect(board.length).toEqual(10);
     board.forEach(row => expect(row.length).toEqual(10));
   });
